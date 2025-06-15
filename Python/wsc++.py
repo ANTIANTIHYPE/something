b243c2e7ea16dfe8994071835845bad94094edd7
YOUWILLDIE666 <youwilldie6.066@gmail.com>
2025-06-15 19:14:11 +0300

    Add files.

--- Python/wsc++.py
+++ Python/wsc++.py
@@ -1,60 +1,62 @@
 ### wsc++.py
 
-# get words .txt from
-# https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_alpha.txt
+#   get words .txt from
+#   https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_alpha.txt
 
 ### 
 
 
 import random
-import string
+from typing import Set, Optional
 
 
-def load_words():
-    with open('words_alpha.txt') as word_file:
-        valid_words = set(word_file.read().split())
-    return valid_words
+def load_words() -> Optional[Set[str]]:
+    try:
+        with open('words_alpha.txt', 'r', encoding='utf-8') as word_file:
+            return set(word_file.read().split())
+    except FileNotFoundError:
+        print("Couldn't find words_alpha.txt. Download it at:\n"
+              "https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_alpha.txt")
+        return None
+
+def separate_punctuation(word: str) -> (str, str):
+    prefix, suffix = '', ''
+    
+    while word and not word[0].isalpha():
+        prefix += word[0]
+        word = word[1:]
+    while word and not word[-1].isalpha():
+        suffix = word[-1] + suffix
+        word = word[:-1]
+
+    return prefix, suffix, word
+
+def process_word(word: str, english_words: Set[str], index: int) -> str:
+    prefix, suffix, clean_word = separate_punctuation(word)
+    if not clean_word:
+        return prefix + suffix
+
+    new_word = [
+        random.choice([w for w in english_words if len(w) > index and w[index] == letter]) 
+        if letter.isalpha() else letter 
+        for letter in clean_word
+    ]
+
+    return prefix + ' '.join(new_word) + suffix
+
 
 if __name__ == '__main__':
+    english_words = load_words()
+    if english_words is None:
+        exit(1)
+
     while True:
-        english_words = load_words()
         sentence = str(input("enter: ")).lower()
-        index = int(input("index: ")) - 1
+        try:
+            index = max(int(input("index: ")) - 1, 0)
+        except ValueError:
+            print("wrong index")
+            continue
 
-        if index < 0:
-            index = 0
-
-        new = []
-
-        # i guess this works for numbers too
-        for word in sentence.split():
-            #separate punctuation
-            prefix = ''
-            suffix = ''
-            if word and not word.isalpha():
-                #handle punctuation
-                while word and not word[0].isalpha():
-                    prefix += word[0]
-                    word = word[1:]
-                while word and not word[-1].isalpha():
-                    suffix = word[-1] + suffix
-                    word = word[:-1]
-
-            if not word:
-                new.append(prefix + suffix)
-                continue
-
-            for letter in word:
-                if letter.isalpha():
-                    matching = [w for w in english_words if len(w) > index and w[index] == letter]
-                    if matching:
-                        new.append(random.choice(matching))
-                    else:
-                        new.append(random.choice(list(english_words)))
-                else:
-                    new.append(letter)
-
-            #reattach garbagew
-            new[-1] = prefix + new[-1] + suffix
-
-        print(' '.join(new))
+        new_sentence = [process_word(word, english_words, index) for word in sentence.split()]
+        print(' '.join(new_sentence))
